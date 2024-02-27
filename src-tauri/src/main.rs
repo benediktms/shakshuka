c5cc867c59e7c8ac82efee0c429a37f4beec78c0
Benedikt Schnatterbeck <benediktms@hey.com>
2024-02-27 09:09:59 +0000

    chore: reformat codebase with prettier

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,53 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, WindowEvent};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
 fn main() {
+    let quit = CustomMenuItem::new("quit", "Quit");
+    let hide = CustomMenuItem::new("hide", "Hide");
+    let show = CustomMenuItem::new("show", "Show");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_item(hide)
+        .add_item(show);
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| {
+            if let SystemTrayEvent::MenuItemClick { id, .. } = event {
+                match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    "hide" => {
+                        let window = app.get_window("main").unwrap();
+                        window.hide().unwrap();
+                    }
+                    "show" => {
+                        let window = app.get_window("main").unwrap();
+                        window.show().unwrap();
+                    }
+                    _ => {}
+                }
+            }
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .on_window_event(|e| {
+            let event = e.event();
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                println!("tried to exit");
+                e.window().hide().expect("could not hide window");
+                api.prevent_close();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
