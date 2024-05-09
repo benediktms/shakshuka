b9cadfc5573c93df5ffb3f7dd7792fcde02c02f6
Benedikt Schnatterbeck <benediktms@hey.com>
2024-05-09 13:48:03 +0100

    feat(system-tray): add system tray support
    The application will be hidden to the system tray by default when
    closing. Quitting via e.g. Cmd+Q will still quit the application though.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, WindowEvent};
+use tauri::{
+    AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, WindowEvent,
+};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn system_tray_event_handler() -> impl Fn(&AppHandle, SystemTrayEvent) {
+    |app, event| {
+        if let SystemTrayEvent::MenuItemClick { id, .. } = event {
+            match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                "show" => {
+                    let window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                _ => {}
+            }
+        }
+    }
 }
 
 fn main() {
@@ -21,24 +44,7 @@ fn main() {
 
     tauri::Builder::default()
         .system_tray(tray)
-        .on_system_tray_event(|app, event| {
-            if let SystemTrayEvent::MenuItemClick { id, .. } = event {
-                match id.as_str() {
-                    "quit" => {
-                        std::process::exit(0);
-                    }
-                    "hide" => {
-                        let window = app.get_window("main").unwrap();
-                        window.hide().unwrap();
-                    }
-                    "show" => {
-                        let window = app.get_window("main").unwrap();
-                        window.show().unwrap();
-                    }
-                    _ => {}
-                }
-            }
-        })
+        .on_system_tray_event(system_tray_event_handler())
         .invoke_handler(tauri::generate_handler![greet])
         .on_window_event(|e| {
             let event = e.event();
