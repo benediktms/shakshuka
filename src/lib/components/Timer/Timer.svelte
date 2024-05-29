2be983792969bf4c9753cc27c078202f263f3f66
Benedikt Schnatterbeck <benediktms@hey.com>
2024-05-29 23:32:15 +0100

    refactor: wrap timer logic into dedicated component

---
+++ src/lib/components/Timer/Timer.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import { Button } from '@/components/ui/button';
+  import RadialProgress from '@/components/Timer/RadialProgress.svelte';
+  import { onDestroy, onMount } from 'svelte';
+
+  export let timeFrame: number;
+
+  type Interval = ReturnType<typeof setInterval>;
+
+  let timePassed: number = 0;
+  let interval: Interval | null = null;
+  let initialTimestamp: number | null = null;
+  let pauseTimestamp: number = 0;
+
+  let isRunning = false;
+
+  const startCount = (): void => {
+    isRunning = true;
+    if (interval) return;
+
+    initialTimestamp = Date.now();
+
+    interval = setInterval(() => {
+      if (initialTimestamp !== null) {
+        timePassed = Date.now() - initialTimestamp + pauseTimestamp;
+      }
+    }, 100);
+  };
+
+  const pauseCount = (): void => {
+    isRunning = false;
+
+    if (initialTimestamp !== null) {
+      pauseTimestamp += Date.now() - initialTimestamp;
+    }
+
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+  };
+
+  const resetCount = (): void => {
+    isRunning = false;
+    if (interval) {
+      clearInterval(interval);
+    }
+    initialTimestamp = null;
+    pauseTimestamp = 0;
+    interval = null;
+    timePassed = 0;
+  };
+
+  onMount(() => {
+    return () => {
+      if (interval !== null) {
+        clearInterval(interval);
+      }
+    };
+  });
+
+  onDestroy(() => {
+    if (interval) {
+      clearInterval(interval);
+    }
+  });
+</script>
+
+<div>
+  <RadialProgress elapsed={Math.floor(timePassed / 1000)} totalMinutes={timeFrame} />
+  {#if isRunning}
+    <Button on:click={pauseCount} disabled={!isRunning}>stop timer</Button>
+  {:else}
+    <Button on:click={startCount} disabled={isRunning}>start timer</Button>
+  {/if}
+  <Button on:click={resetCount}>reset timer</Button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,78 +1,8 @@
 <script lang="ts">
-  import { Button } from '@/components/ui/button';
-  import RadialProgress from '@/components/RadialProgress.svelte';
-  import { onDestroy, onMount } from 'svelte';
-
-  export let timeFrame: number;
-
-  type Interval = ReturnType<typeof setInterval>;
-
-  let timePassed: number = 0;
-  let interval: Interval | null = null;
-  let initialTimestamp: number | null = null;
-  let pauseTimestamp: number = 0;
-
-  let isRunning = false;
-
-  const startCount = (): void => {
-    isRunning = true;
-    if (interval) return;
-
-    initialTimestamp = Date.now();
-
-    interval = setInterval(() => {
-      if (initialTimestamp !== null) {
-        timePassed = Date.now() - initialTimestamp + pauseTimestamp;
-      }
-    }, 100);
-  };
-
-  const pauseCount = (): void => {
-    isRunning = false;
-
-    if (initialTimestamp !== null) {
-      pauseTimestamp += Date.now() - initialTimestamp;
-    }
-
-    if (interval) {
-      clearInterval(interval);
-      interval = null;
-    }
-  };
-
-  const resetCount = (): void => {
-    isRunning = false;
-    if (interval) {
-      clearInterval(interval);
-    }
-    initialTimestamp = null;
-    pauseTimestamp = 0;
-    interval = null;
-    timePassed = 0;
-  };
-
-  onMount(() => {
-    return () => {
-      if (interval !== null) {
-        clearInterval(interval);
-      }
-    };
-  });
-
-  onDestroy(() => {
-    if (interval) {
-      clearInterval(interval);
-    }
-  });
+  import Timer from '@/components/Timer/Timer.svelte';
 </script>
 
 <div class="container">
   <h1>Welcome to Shakshuka</h1>
-  <RadialProgress elapsed={Math.floor(timePassed / 1000)} totalMinutes={timeFrame} />
-  {#if isRunning}
-    <Button on:click={pauseCount} disabled={!isRunning}>stop timer</Button>
-  {:else}
-    <Button on:click={startCount} disabled={isRunning}>start timer</Button>
-  {/if}
-  <Button on:click={resetCount}>reset timer</Button>
+  <Timer timeFrame={30} />
 </div>
