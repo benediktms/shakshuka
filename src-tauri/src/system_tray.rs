c4afe134f0a200e202b3a7e81c85aca2bcfa53d6
Benedikt Schnatterbeck <benediktms@hey.com>
2024-06-07 11:38:25 +0100

    refactor: also move system tray menu to module

--- src-tauri/src/system_tray.rs
+++ src-tauri/src/system_tray.rs
@@ -1,4 +1,13 @@
-use tauri::{AppHandle, Manager, SystemTrayEvent};
+use tauri::{AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
+pub fn make_system_tray() -> SystemTray {
+    SystemTray::new().with_menu(
+        SystemTrayMenu::new()
+            .add_item(CustomMenuItem::new("quit", "Quit"))
+            .add_item(CustomMenuItem::new("hide", "Hide"))
+            .add_item(CustomMenuItem::new("show", "Show")),
+    )
+}
 
 pub fn system_tray_event_handler() -> impl Fn(&AppHandle, SystemTrayEvent) {
     |app, event| {
