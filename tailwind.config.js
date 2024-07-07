3ac531e9152367f2ba922c72ef597a28ad307b1c
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-07 22:30:11 +0100

    style: add css variable for window decorations

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ const config = {
         ring: 'hsl(var(--ring) / <alpha-value>)',
         background: 'hsl(var(--background) / <alpha-value>)',
         foreground: 'hsl(var(--foreground) / <alpha-value>)',
+        decoration: 'hsl(var(--decoration) / <alpha-value>)',
         primary: {
           DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
           foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
