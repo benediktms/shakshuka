63e4d1f24e74508e32bef4e453b118d6e4213f11
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-07 20:26:18 +0100

    style: use green catppucin theme for light and dark mode
    also simplify the logic for determining the current theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,22 @@
 <script lang="ts">
   import '../app.pcss';
-  import { onMount } from 'svelte';
+  import { onDestroy, onMount } from 'svelte';
   import { theme } from '$lib/globalStores/themeStore';
 
+  const changeHandler = (e: MediaQueryListEvent) => {
+    $theme = e.matches ? 'dark' : 'light';
+  };
+
+  const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
+  $theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+  $: document.documentElement.classList.toggle('dark', $theme === 'dark');
+
   onMount(() => {
-    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'dark'
-      : 'light';
-    theme.set(systemTheme);
-
-    const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
-    const changeHandler = (e: MediaQueryListEvent) => {
-      theme.set(e.matches ? 'dark' : 'light');
-    };
     mediaQueryList.addEventListener('change', changeHandler);
-
-    const unsubscribe = theme.subscribe(value => {
-      document.documentElement.classList[value === 'dark' ? 'add' : 'remove']('dark');
-    });
-
-    return () => {
-      unsubscribe();
-      mediaQueryList.removeEventListener('change', changeHandler);
-    };
   });
+  onDestroy(() => mediaQueryList.removeEventListener('change', changeHandler));
 </script>
 
 <div class="h-dvh">
